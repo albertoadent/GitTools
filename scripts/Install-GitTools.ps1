b7a36bbc342ef2da7646ac09208b6ecd8b2907c3
Alberto Dent <albertoadent@gmail.com>
2025-06-25 15:52:41 -0400

    fixed typo

--- scripts/Install-GitTools.ps1
+++ scripts/Install-GitTools.ps1
@@ -6,12 +6,12 @@ param(
 $modulePath = Join-Path $env:PSModulePath "GitTools"
 $repoUrl = "https://github.com/albertoadent/GitTools.git"
 
-if(-not Get-Command git -ErrorAction SilentlyContinue) {
+if(-not (Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Error "Git is not installed. Installing Git..."
     winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements
     
     Start-Sleep -Seconds 10
-    if(-not Get-Command git -ErrorAction SilentlyContinue) {
+    if(-not (Get-Command git -ErrorAction SilentlyContinue)) {
         Write-Error "Git is not installed. Please install Git and try again."
         exit 1
     }
